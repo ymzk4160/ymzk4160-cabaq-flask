0ec9a97fa367b79483552f00150a066d0d02fe26
ymzk4160 <yoichiro@oregano.ocn.ne.jp>
2025-05-03 21:42:26 +0900

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+# from flask_login import LoginManager  # コメントアウト
 
 # データベース
 db = SQLAlchemy()
@@ -9,8 +9,8 @@ db = SQLAlchemy()
 migrate = Migrate()
 # パスワードハッシュ化
 bcrypt = Bcrypt()
-# ログイン管理
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.login_message = 'この機能を使用するにはログインが必要です。'
-login_manager.login_message_category = 'info'
+# ログイン管理（すべてコメントアウト）
+# login_manager = LoginManager()
+# login_manager.login_view = 'auth.login'
+# login_manager.login_message = 'この機能を使用するにはログインが必要です。'
+# login_manager.login_message_category = 'info'
