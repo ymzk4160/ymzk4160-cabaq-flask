b6c3e2258bcd4ca378f3a6e6f2e333e5ffdec7d3
ymzk4160 <yoichiro@oregano.ocn.ne.jp>
2025-05-03 23:13:56 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from app.routes import main  # Blueprint をインポート
+
 
 def create_app():
     app = Flask(__name__)
 
-    # ルーティング等の設定…
+    # Blueprint を登録
+    app.register_blueprint(main.bp)
 
+    # 404 エラーハンドラー
     @app.errorhandler(404)
     def page_not_found(e):
         return render_template('errors/404.html'), 404
