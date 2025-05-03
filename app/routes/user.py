7c2568abd399bbdc9aed018d76d4a852112ed47f
ymzk4160 <yoichiro@oregano.ocn.ne.jp>
2025-05-03 21:57:05 +0900

    Update user.py

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,1 +1,11 @@
- 
+from datetime import datetime
+# from flask_login import UserMixin  # コメントアウト
+from sqlalchemy.dialects.postgresql import JSON
+from app.extensions import db  # login_managerを削除
+
+# @login_manager.user_loader  # コメントアウト
+# def load_user(user_id):     # コメントアウト
+#     return User.query.get(int(user_id))  # コメントアウト
+
+class User(db.Model):  # UserMixinを削除
+    # 既存のコード...
