1057b011b736275f426709e21089a179bfc481c5
ymzk4160 <yoichiro@oregano.ocn.ne.jp>
2025-05-04 13:11:39 +0900

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,8 @@
-# モデルをインポート
+# app/models/__init__.py
+
+# モデルを明示的にインポートして二重登録を防ぐ
 from app.models.user import User
-from app.models.content import Question, Answer
+from app.models.question import Question
+from app.models.answer import Answer
+
+__all__ = ['User', 'Question', 'Answer']
