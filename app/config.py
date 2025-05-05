fa500cdb471a3a7a2e7adf35862b165d9730b235
ymzk4160 <yoichiro@oregano.ocn.ne.jp>
2025-05-05 12:28:14 +0900

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    # データベース設定
+    # ホスト名を完全な形式で指定
     SQLALCHEMY_DATABASE_URI = 'postgresql://cabaq_user:mw7CAB5ScAnTd1OvVNylYhnny2GGUozL@dpg-d0aaijruibrs73bmdp60-a.oregon-postgres.render.com/cabaq'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
