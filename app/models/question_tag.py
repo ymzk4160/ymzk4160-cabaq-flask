0cc161ee739353c2191fb2af1837bd1373bc7065
ymzk4160 <yoichiro@oregano.ocn.ne.jp>
2025-05-05 13:03:55 +0900

    Update question_tag.py

--- app/models/question_tag.py
+++ app/models/question_tag.py
@@ -1,1 +1,15 @@
+from datetime import datetime
+from app.extensions import db
 
+class QuestionTag(db.Model):
+    __tablename__ = 'question_tags'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'), nullable=False)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    
+    # リレーション設定
+    question = db.relationship('Question', backref=db.backref('question_tags', lazy=True))
+    tag = db.relationship('Tag', backref=db.backref('question_tags', lazy=True))
