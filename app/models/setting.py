069b56330966b81b40be8f58aed762ff39fb186b
ymzk4160 <yoichiro@oregano.ocn.ne.jp>
2025-05-05 14:05:13 +0900

    Update setting.py

--- app/models/setting.py
+++ app/models/setting.py
@@ -1,1 +1,16 @@
+from datetime import datetime
+from app.extensions import db
 
+class Setting(db.Model):
+    __tablename__ = 'settings'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(100), nullable=False, unique=True)
+    value = db.Column(db.Text)
+    type = db.Column(db.String(20))
+    group = db.Column(db.String(50))
+    description = db.Column(db.Text)
+    is_public = db.Column(db.Boolean, default=True)
+    updated_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
