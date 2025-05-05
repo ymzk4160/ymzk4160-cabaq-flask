918f6e30ad41be48a87cec00d6ad0c5c1c5960db
ymzk4160 <yoichiro@oregano.ocn.ne.jp>
2025-05-05 12:24:49 +0900

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     # データベース設定
-    SQLALCHEMY_DATABASE_URI = 'postgresql://cabaq_user:mw7CAB5ScAnTd1OvVNylYhnny2GGUozL@dpg-d0aaijruibrs73bmdp60-a/cabaq'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://cabaq_user:mw7CAB5ScAnTd1OvVNylYhnny2GGUozL@dpg-d0aaijruibrs73bmdp60-a.oregon-postgres.render.com/cabaq'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
